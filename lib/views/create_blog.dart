3710a90c3d4d9bd2947c5ea0bd600be455d854fb
hj <dv.zinke@gmail.com>
2020-09-17 21:39:56 +0900

    feat : 파이어베이스에 데이터 삽입

--- lib/views/create_blog.dart
+++ lib/views/create_blog.dart
@@ -1,4 +1,10 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_blog_app/services/crud.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:random_string/random_string.dart';
 
 class CreateBlog extends StatefulWidget {
   @override
@@ -7,6 +13,51 @@ class CreateBlog extends StatefulWidget {
 
 class _CreateBlogState extends State<CreateBlog> {
   String authorName, title, desc;
+
+  CrudMethods crudMethods = new CrudMethods();
+  bool _isLoading = false;
+
+  File selectedImage;
+
+  final picker = ImagePicker();
+
+  Future getImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+
+    setState(() {
+      selectedImage = File(pickedFile.path);
+    });
+  }
+
+  uploadBlog() async {
+    if (selectedImage != null) {
+
+      setState((){
+        _isLoading = true;
+      });
+      StorageReference firebaseStorageRef = FirebaseStorage.instance
+          .ref()
+          .child("blogImages")
+          .child("${randomAlphaNumeric(9)}");
+
+      final StorageUploadTask task = firebaseStorageRef.putFile(selectedImage);
+
+      var downloadUrl = await (await task.onComplete).ref.getDownloadURL();
+      print("this is url$downloadUrl");
+
+      Map<String, String> blogMap = {
+        "image" : downloadUrl,
+        "name" : authorName,
+        "title" : title,
+        "desc" : desc
+      };
+
+      crudMethods.addData(blogMap).then( (result){
+        Navigator.pop(context);
+      });
+
+    } else {}
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,57 +76,84 @@ class _CreateBlogState extends State<CreateBlog> {
           backgroundColor: Colors.transparent,
           elevation: 0,
           actions: [
-            Container(
-              child: Icon(Icons.file_upload),
-              padding: EdgeInsets.symmetric(horizontal: 10),
+            GestureDetector(
+              onTap: () {
+                uploadBlog();
+              },
+              child: Container(
+                child: Icon(Icons.file_upload),
+                padding: EdgeInsets.symmetric(horizontal: 10),
+              ),
             )
           ],
         ),
-        body: Container(
-          child: Column(
-            children: [
-              SizedBox(height: 10),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 16),
-                height: 150,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(6)),
-                child: Icon(
-                  Icons.add_a_photo,
-                  color: Colors.black45,
-                ),
-              ),
-              SizedBox(
-                height: 8,
-              ),
-              Container(
+        body: _isLoading
+            ? Container(
+                child: CircularProgressIndicator(),
+                alignment: Alignment.center,
+              )
+            : Container(
                 child: Column(
                   children: [
-                    TextField(
-                      decoration: InputDecoration(hintText: "이름"),
-                      onChanged: (val) {
-                        authorName = val;
+                    SizedBox(height: 10),
+                    GestureDetector(
+                      onTap: () {
+                        getImage();
                       },
+                      child: selectedImage != null
+                          ? Container(
+                              padding: EdgeInsets.symmetric(horizontal: 16),
+                              height: 150,
+                              child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(6),
+                                  child: Image.file(
+                                    selectedImage,
+                                    fit: BoxFit.cover,
+                                  )),
+                              width: MediaQuery.of(context).size.width,
+                            )
+                          : Container(
+                              padding: EdgeInsets.symmetric(horizontal: 16),
+                              height: 150,
+                              width: MediaQuery.of(context).size.width,
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(6)),
+                              child: Icon(
+                                Icons.add_a_photo,
+                                color: Colors.black45,
+                              ),
+                            ),
                     ),
-                    TextField(
-                      decoration: InputDecoration(hintText: "제목"),
-                      onChanged: (val) {
-                        title = val;
-                      },
+                    SizedBox(
+                      height: 8,
                     ),
-                    TextField(
-                      decoration: InputDecoration(hintText: "상세 내용"),
-                      onChanged: (val) {
-                        desc = val;
-                      },
+                    Container(
+                      child: Column(
+                        children: [
+                          TextField(
+                            decoration: InputDecoration(hintText: "이름"),
+                            onChanged: (val) {
+                              authorName = val;
+                            },
+                          ),
+                          TextField(
+                            decoration: InputDecoration(hintText: "제목"),
+                            onChanged: (val) {
+                              title = val;
+                            },
+                          ),
+                          TextField(
+                            decoration: InputDecoration(hintText: "상세 내용"),
+                            onChanged: (val) {
+                              desc = val;
+                            },
+                          )
+                        ],
+                      ),
                     )
                   ],
                 ),
-              )
-            ],
-          ),
-        ));
+              ));
   }
 }
