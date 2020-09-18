e9de6c9eaaa7634a161e20fa598cf9ed47df7d06
hj <dv.zinke@gmail.com>
2020-09-18 23:20:22 +0900

    feat : getData 추가 및 코드 수정

--- lib/services/crud.dart
+++ lib/services/crud.dart
@@ -4,8 +4,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class CrudMethods {
 
   Future<void> addData(blogData) async {
-    Firestore.instance.collection("blogs").add(blogData).catchError((e){
+    FirebaseFirestore.instance.collection("blogs").add(blogData).catchError((e){
       print(e);
     });
+  }
+
+  getData() async {
+    return await FirebaseFirestore.instance.collection("blogs").get();
   }
 }
